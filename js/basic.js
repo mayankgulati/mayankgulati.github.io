56771434a2a6f6a0a2255b9763b8efd7d0859fa0
Mayank Gulati <mayankgulati42@gmail.com>
2016-06-16 14:02:39 +0530

    Added website

---
+++ js/basic.js
@@ -0,0 +1,24 @@
+function toOrange(){
+    $("#cn").attr('src', 'img/cn_or.png');
+    $('#ctext').text("My Startup");
+}
+function toBlack(){
+    $("#cn").attr('src', 'img/cn_bl.png');
+    $('#ctext').text("ContentNinja.in");
+}
+function ghub1(){
+   	$("#github").attr('src','img/gh_wh.png');
+    $('#gtext').text("My Code");
+}
+function ghub2(){
+   	$("#github").attr('src','img/gh_bl.png');
+   	$('#gtext').text("/mayankgulati");
+}
+function lin1(){
+   	$("#linkedin").attr('src','img/li_bu.png');
+   	$('#ltext').text("My Portfolio");
+}
+function lin2(){
+   	$("#linkedin").attr('src','img/li_wh.png');
+   	$('#ltext').text("/in/gulatimayank");
+}
